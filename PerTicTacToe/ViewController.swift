822a4bd90e4e596b28c5dcd0ac004654ca9f065f
BettinaP <bettinaprophete@gmail.com>
2018-02-27 18:54:26 -0500

    first commit

---
+++ PerTicTacToe/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PerTicTacToe
+//
+//  Created by Bettina on 2/27/18.
+//  Copyright © 2018 Bettina Prophete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+}
+
