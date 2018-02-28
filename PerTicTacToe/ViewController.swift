c238086b121210bd52ad879cb932a3307df6ac4a
BettinaP <bettinaprophete@gmail.com>
2018-02-27 21:05:35 -0500

    set up players and tracking of whether spot on board is occupied

--- PerTicTacToe/ViewController.swift
+++ PerTicTacToe/ViewController.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //1 is noughts, 2 is crosses
+    var activePlayer = 1
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 - empty spot, 1 - noughts/O, 2 - crosses/X
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        sender.setImage(UIImage(named: "nought.png"), for: [])
+        //slight disadvantage of using tag 1-9 because array index starts with 0.
+        let activePosition = sender.tag - 1
         
-        print(sender.tag)
+        if gameState[activePosition] == 0 {
+            
+            gameState[activePosition] = activePlayer
+            
+            if activePlayer == 1 {
+                
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                activePlayer = 2
+            
+            } else {
+                
+                sender.setImage(UIImage(named: "cross.png"), for: [])
+                activePlayer = 1
+                
+            }
+        }
     }
     
 }
