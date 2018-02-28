2c22e71dabc017dd7be34e2c90d76111d1818273
BettinaP <bettinaprophete@gmail.com>
2018-02-27 21:23:39 -0500

    setting up winning combos

--- PerTicTacToe/ViewController.swift
+++ PerTicTacToe/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     //1 is noughts, 2 is crosses
     var activePlayer = 1
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 - empty spot, 1 - noughts/O, 2 - crosses/X
+    let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    var activeGame = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,7 @@ class ViewController: UIViewController {
         
         if gameState[activePosition] == 0 {
             
-            gameState[activePosition] = activePlayer
+            gameState[activePosition] = activePlayer //switches from 0(empty) to whichever player occupies it
             
             if activePlayer == 1 {
                 
@@ -42,6 +44,16 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "cross.png"), for: [])
                 activePlayer = 1
                 
+            }
+            
+            for combination in winningCombinations {
+                //if gameState == 0 in any position, no win yet. Then checking for gameState to be 1,1,1 or 2,2,2
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    //we have a winner! NOtify users and give them option to play again.
+                    
+                    
+                }
             }
         }
     }
